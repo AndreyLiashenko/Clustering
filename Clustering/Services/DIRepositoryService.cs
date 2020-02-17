645c6540295ef34e9499d3d0e7ab5e6b6b51bfcc
Андрей Ляшенко <andrey.lyashenko44@gmail.com>
2020-02-17 14:41:30 +0200

    add to DI Container

---
+++ Clustering/Services/DIRepositoryService.cs
@@ -0,0 +1,17 @@
+﻿using Clustering.Services.CsvRead;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Clustering.Services
+{
+    public static class DIRepositoryService
+    {
+        public static void AddRepositories(this IServiceCollection services)
+        {
+            services.AddScoped<IGetScvRows, GetCsvRows>();
+        }
+    }
+}
